cdb51ad791e73bc54bb30df1ba1c06cc07fd95c6
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-11-05 18:33:15 +0900

    カウントを増やす機能を実装

--- react_ts_app/src/App.tsx
+++ react_ts_app/src/App.tsx
@@ -8,6 +8,13 @@ import './App.css';
 function App():ReactElement {
   // ステート変数
   const [ msg, setMsg ] = useState("This is sample message.")
+  const [ count, setCount ] = useState(0)
+
+  // countを増やす関数
+  const doAction = () =>{
+    setCount(count + 1)
+  }
+
   return (
     <div>
       <h1 className="bg-primary text-while p-2">React Sample</h1>
@@ -15,7 +22,10 @@ function App():ReactElement {
         <h2 className="my-3">click button!</h2>
         <div className="alert alert-primary">
           <div className="row px-2">
-            <h3 id="msg">{msg}</h3>
+            <h3 id="msg" className="col">{count} click.</h3>
+            <button onClick={doAction} className="btn btn-primary col-2">
+              Click!
+            </button>
           </div>
         </div>
       </div>
