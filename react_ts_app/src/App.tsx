ee7c5c98b4223fefc7b0995abfd6498fa2a95b38
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-11-05 18:29:34 +0900

    メッセージを変更

--- react_ts_app/src/App.tsx
+++ react_ts_app/src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+/**
+ * メインコンポーネントファイル
+ */
+
+import React, { useState, ReactElement } from 'react';
 import './App.css';
 
-function App() {
+function App():ReactElement {
+  // ステート変数
+  const [ msg, setMsg ] = useState("This is sample message.")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="bg-primary text-while p-2">React Sample</h1>
+      <div className="container">
+        <h2 className="my-3">click button!</h2>
+        <div className="alert alert-primary">
+          <div className="row px-2">
+            <h3 id="msg">{msg}</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
